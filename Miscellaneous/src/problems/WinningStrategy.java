aa3c087df219dfaf836364df52aec8f046f5f0c3
aditya301200 <Aditya.Das2021@iem.edu.in>
2024-02-28 10:34:04 +0530

    second commit

--- Miscellaneous/src/problems/WinningStrategy.java
+++ Miscellaneous/src/problems/WinningStrategy.java
@@ -11,7 +11,7 @@ There are n players in our team, numbered from 1 to n.
 The coach will know the position of another team hence create a winning strategy.
 He creates the position of every player in a specific order so that we will win and
 then he starts swapping two players at a time to form the positions.
-He swaps payers in such a way that it can't be understood by another team:
+He swaps players in such a way that it can't be understood by another team:
 1. Any player can swap with the player directly at front him
 2. One player can swap at most with two other players
 If the specific order is formed then our team will win otherwise we will lose
@@ -51,12 +51,47 @@ In the First Sample case,
 In the second case, no way to form this specific order
  */
 public class WinningStrategy {
+    private static boolean strategy(int[] arr, int n){
+        int cnt = 0;
+        for(int i=n-1; i>=2; i--){
+            if(arr[i]==i+1){
+                continue;
+            } else if(arr[i-2]==i+1){
+                int temp = arr[i];
+                arr[i] = arr[i-2];
+                arr[i-2] = temp;
+                cnt+=2;
+            } else if(arr[i-1] == i+1){
+                int temp = arr[i];
+                arr[i] = arr[i-1];
+                arr[i-1] = temp;
+                cnt++;
+            } else{
+                return false;
+            }
+        }
+        if(arr[0] == 1 && arr[1] == 2){
+            return true;
+        } else if(arr[0]==2 && arr[1]==1){
+            arr[0] = 1;
+            arr[1] = 2;
+            cnt++;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i=0; i<n; i++) {
             arr[i] = sc.nextInt();
+        }
+        sc.close();
+        boolean ans = strategy(arr,n);
+        if(ans){
+            System.out.println("Yes");
+        } else{
+            System.out.println("No");
         }
     }
 }
